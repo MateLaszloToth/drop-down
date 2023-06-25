a193bc030b774373f8a70e5a82e087804f87b257
Mate Toth <mate.toth@kambr.com>
2023-06-25 11:48:57 -0700

    implement slideshow

--- controller.js
+++ controller.js
@@ -1,6 +1,8 @@
 import Slider from "./slider.js";
 
 const Controller = (() => {
+    const sliderFrame = document.querySelector('div.slider > div.frame > ul.slider-frame');
+    const slideMarkers = document.querySelector('div.slider > div.frame > ul.marker-frame');
 
     const SECOND_IN_MILLIS = 1000;
     const startSlideShow = () => {
@@ -19,13 +21,42 @@ const Controller = (() => {
     }
 
     const addEventListenerToMarkers = () => {
-        const markerElements = [...document.querySelector('ul.slide-markers > li')];
+        const markerElements = [...slideMarkers.children];
         for (const marker of markerElements) {
             marker.addEventListener('click', e => Slider.showChosenSlide(e))
         }
     }
 
+    const createSlideElements = () => {
+        const slides = Slider.getSlides().map(slide => {
+                const img = document.createElement('img');
+                img.alt = slide.alt;
+                img.src = slide.url
+
+                const li = document.createElement('li');
+                li.classList.add('slide');
+                li.append(img);
+                return li;
+            }
+        )
+        slides[0].classList.add('visible');
+        return slides;
+    }
+
+    const createSlideMarkerElements = () => {
+        const markers = Slider.getSlides().map((_, index) => {
+            const marker = document.createElement('li');
+            marker.classList.add('slide-marker');
+            marker.setAttribute('data-slider-index', index);
+            return marker;
+        })
+        markers[0].classList.add('chosen');
+        return markers;
+    }
+
     const initializeSlideShow = () => {
+        sliderFrame.replaceChildren(...createSlideElements());
+        slideMarkers.replaceChildren(...createSlideMarkerElements());
         addEventListenerToArrows();
         addEventListenerToMarkers();
         startSlideShow();
--- slider.js
+++ slider.js
@@ -2,48 +2,66 @@ import Slide from './slide.js';
 
 const Slider = (() => {
 
-    const pictures = [
+    const slides = [
         Slide('https://wallpapercave.com/wp/1ZLpfHy.jpg', 'Sunset over the ocean'),
         Slide('http://www.pixelstalk.net/wp-content/uploads/2016/08/Cool-Nature-Background-Images-4523x2590.jpg', 'Palm trees hanging over the ocean'),
         Slide('http://wallpapercave.com/wp/20niwZQ.jpg', 'Niagara waterfall'),
         Slide('https://www.pixelstalk.net/wp-content/uploads/2016/08/Free-HD-Beautiful-Backgrounds-For-PC.jpg', 'Red blue forest')
     ]
 
-    const markers = [...document.querySelectorAll('li.slide-marker')];
+    const sliderFrameElement = document.querySelector('div.slider > div.frame > ul.slider-frame');
+
+    const getSlideElements = () => [...document.querySelectorAll('div.slider li.slide')];
 
     const updateMarker = newMarkerIndex => {
-        for(const marker of markers) { marker.classList.remove('chosen'); }
-        markers.find(it => it.getAttribute('data-slider-index') === newMarkerIndex).classList.add('chosen');
+        const markerElements = [...document.querySelectorAll('li.slide-marker')];
+        for (const marker of markerElements) {
+            marker.classList.remove('chosen');
+        }
+        markerElements[newMarkerIndex].classList.add('chosen');
+        const sliderMarkerFrameElement = document.querySelector('div.slider > div.frame > ul.marker-frame');
+        sliderMarkerFrameElement.replaceChildren(...markerElements);
     }
     const showNextSlide = () => {
-        const currentSlide = getCurrentSlide();
-        currentSlide.classList.remove('visible');
-        const currentSlideIndex = (pictures.findIndex(slide => slide.url === currentSlide.url)).toString();
-        const nextSlideIndex = ((currentSlideIndex + 1) % pictures.length).toString();
-        pictures[nextSlideIndex].classList.add('visible');
+        const slideImageElements = getSlideElements();
+        const currentSlideImage = getCurrentSlideElement();
+        const currentSlideIndex = slideImageElements.findIndex(slide => slide.firstChild.src === currentSlideImage.src);
+        slideImageElements[currentSlideIndex].classList.remove('visible');
+        const nextSlideIndex = ((currentSlideIndex + 1) % slideImageElements.length).toString();
+        slideImageElements[nextSlideIndex].classList.add('visible');
+        sliderFrameElement.replaceChildren(...slideImageElements);
         updateMarker(nextSlideIndex);
     }
 
     const showPreviousSlide = () => {
-        const currentSlide = getCurrentSlide();
-        currentSlide.classList.remove('visible');
-        const currentSlideIndex = (pictures.findIndex(slide => slide.url === currentSlide.url)).toString();
-        const previousSlideIndex = ((pictures.length + currentSlideIndex - 1) % pictures.length).toString();
-        pictures[previousSlideIndex].classList.add('visible');
+        const slideImageElements = getSlideElements();
+        const currentSlideImage = getCurrentSlideElement();
+        const currentSlideIndex = slideImageElements.findIndex(slide => slide.firstChild.src === currentSlideImage.src);
+        slideImageElements[currentSlideIndex].classList.remove('visible');
+        const previousSlideIndex = ((slideImageElements.length + currentSlideIndex - 1) % slideImageElements.length).toString();
+        slideImageElements[previousSlideIndex].classList.add('visible');
+        sliderFrameElement.replaceChildren(...slideImageElements);
         updateMarker(previousSlideIndex);
     }
 
     const showChosenSlide = event => {
-        const currentSlide = getCurrentSlide();
-        currentSlide.classList.remove('visible');
-        const chosenSliderIndex = event.currentTarget.getAttribute('data-slider-index');
-        pictures[chosenSliderIndex].classList.add('visible');
+        event.stopPropagation();
+        const slideImageElements = getSlideElements();
+        const currentSlideImage = getCurrentSlideElement();
+        const currentSlideIndex = slideImageElements.findIndex(slide => slide.firstChild.src === currentSlideImage.src);
+        const chosenSliderIndex = parseInt(event.currentTarget.getAttribute('data-slider-index'));
+        slideImageElements[currentSlideIndex].classList.remove('visible');
+        slideImageElements[chosenSliderIndex].classList.add('visible');
+        sliderFrameElement.replaceChildren(...slideImageElements);
         updateMarker(chosenSliderIndex);
     }
 
-    const getCurrentSlide = () => document.querySelector('div.visible');
+    const getSlides = () => [...slides];
 
-    return {showNextSlide, showPreviousSlide, showChosenSlide}
+
+    const getCurrentSlideElement = () => document.querySelector('li.visible > img');
+
+    return {showNextSlide, showPreviousSlide, showChosenSlide, getSlides}
 })();
 
 export default Slider;

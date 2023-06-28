31543c459c8a926fb3f20284e81c5c8caeeeb17d
Mate Toth <mate.toth@kambr.com>
2023-06-27 19:31:57 -0700

    implement slide fading in and out

--- slider.js
+++ slider.js
@@ -3,10 +3,10 @@ import Slide from './slide.js';
 const Slider = (() => {
 
     const slides = [
-        Slide('https://wallpapercave.com/wp/1ZLpfHy.jpg', 'Sunset over the ocean'),
-        Slide('http://www.pixelstalk.net/wp-content/uploads/2016/08/Cool-Nature-Background-Images-4523x2590.jpg', 'Palm trees hanging over the ocean'),
-        Slide('http://wallpapercave.com/wp/20niwZQ.jpg', 'Niagara waterfall'),
-        Slide('https://www.pixelstalk.net/wp-content/uploads/2016/08/Free-HD-Beautiful-Backgrounds-For-PC.jpg', 'Red blue forest')
+        Slide('sunset.jpg', 'Sunset over the ocean'),
+        Slide('palmtrees.jpg', 'Palm trees hanging over the ocean'),
+        Slide('niagara.jpg', 'Niagara waterfall'),
+        Slide('forest.jpg', 'Red blue forest')
     ]
 
     const sliderFrameElement = document.querySelector('div.slider > div.frame > ul.slider-frame');
@@ -29,7 +29,6 @@ const Slider = (() => {
         slideImageElements[currentSlideIndex].classList.remove('visible');
         const nextSlideIndex = ((currentSlideIndex + 1) % slideImageElements.length).toString();
         slideImageElements[nextSlideIndex].classList.add('visible');
-        sliderFrameElement.replaceChildren(...slideImageElements);
         updateMarker(nextSlideIndex);
     }
 
@@ -40,7 +39,6 @@ const Slider = (() => {
         slideImageElements[currentSlideIndex].classList.remove('visible');
         const previousSlideIndex = ((slideImageElements.length + currentSlideIndex - 1) % slideImageElements.length).toString();
         slideImageElements[previousSlideIndex].classList.add('visible');
-        sliderFrameElement.replaceChildren(...slideImageElements);
         updateMarker(previousSlideIndex);
     }
 
@@ -52,7 +50,6 @@ const Slider = (() => {
         const chosenSliderIndex = parseInt(event.currentTarget.getAttribute('data-slider-index'));
         slideImageElements[currentSlideIndex].classList.remove('visible');
         slideImageElements[chosenSliderIndex].classList.add('visible');
-        sliderFrameElement.replaceChildren(...slideImageElements);
         updateMarker(chosenSliderIndex);
     }
 
